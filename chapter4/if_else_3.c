5fd20ee1f5b57bc299d1c12321bf0e9de9b59dc4
bobchin <bobchin.ryu@gmail.com>
2018-03-15 12:06:22 +0900

    4-3までを追加

---
+++ chapter4/if_else_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int a = 1;
+    // int a = 0;
+
+    if (a != 0)
+        printf("a is %d\n", a);
+        // 書き方がおかしい
+        printf("a have value\n");
+    
+    return 0;
+}
