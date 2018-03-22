a7699d297ff0a5603d6919f3cb63f78ec0c2c592
bobchin <bobchin.ryu@gmail.com>
2018-03-22 18:00:21 +0900

    10-4までを追加

---
+++ chapter10/memory_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    char *ptr;
+
+    ptr = malloc(2147483647);
+    if (ptr == NULL) {
+        printf("Memory allocation error!\n");
+        return -1;
+    }
+    getchar();
+
+    for (i = 0; i < 2147483647; i++) {
+        ptr[i] = 1;
+    }
+    free(ptr);
+
+    return 0;
+}
+
